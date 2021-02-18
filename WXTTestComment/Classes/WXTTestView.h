a63796e85648b41fdae43d3f422c947311093d09
weixuetao <weixuetao@5i5j.com>
2021-02-18 13:14:33 +0800

    组件化测试

---
+++ WXTTestComment/Classes/WXTTestView.h
@@ -0,0 +1,17 @@
+//
+//  WXTTestView.h
+//  WXTTestComment_Example
+//
+//  Created by 魏学涛 on 2021/2/18.
+//  Copyright © 2021 weixuetao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WXTTestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WXTTestComment/Classes/WXTTestView.m
@@ -0,0 +1,21 @@
+//
+//  WXTTestView.m
+//  WXTTestComment_Example
+//
+//  Created by 魏学涛 on 2021/2/18.
+//  Copyright © 2021 weixuetao. All rights reserved.
+//
+
+#import "WXTTestView.h"
+
+@implementation WXTTestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
